9e66d68936b9752a400f4c5372f554e1827e8192
ARNAB DASH <adash92@users.noreply.github.com>
2017-12-25 18:09:41 +0530

    Update 02_NAMED_ENTITY_API_2.R

--- 02_NAMED_ENTITY_API_2.R
+++ 02_NAMED_ENTITY_API_2.R
@@ -13,35 +13,35 @@
 
 #Collecting data from New York Times API for the most frequent named entities discovered from Twitter API
 #Data for named entity - person
-person_entity_API_2 <- as_search(q = person_entity_final, all_results = TRUE,begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "1d5bcdd3e4ef463695e9f1037781fec6",sleep = 1)
+person_entity_API_2 <- as_search(q = person_entity_final, all_results = TRUE,begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "*****",sleep = 1)
 person_entity_API_2_data <- person_entity_API_2$data
 person_entity_API_2_final <- person_entity_API_2_data[,2]
 colnames(person_entity_API_2_final)<-c("person")
 #View(person_entity_API_2_final)
   
 #Data for named entity - location
-location_entity_API_2 <- as_search(q = location_entity_final, all_results = TRUE, begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "1d5bcdd3e4ef463695e9f1037781fec6",sleep = 1)
+location_entity_API_2 <- as_search(q = location_entity_final, all_results = TRUE, begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "*****",sleep = 1)
 location_entity_API_2_data <- location_entity_API_2$data
 location_entity_API_2_final <- location_entity_API_2_data[,2]
 colnames(location_entity_API_2_final)<-c("location")
 #View(location_entity_API_2_final)
 
 #Data for named entity - organization
-organization_entity_API_2 <- as_search(q = organization_entity_final, all_results = TRUE, begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "1d5bcdd3e4ef463695e9f1037781fec6",sleep = 1)
+organization_entity_API_2 <- as_search(q = organization_entity_final, all_results = TRUE, begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "*****",sleep = 1)
 organization_entity_API_2_data <- organization_entity_API_2$data
 organization_entity_API_2_final <- organization_entity_API_2_data[,2]
 colnames(organization_entity_API_2_final)<-c("organization")
 #View(organization_entity_API_2_final)
 
 #Data for named entity - money/currency
-money_entity_API_2 <- as_search(q = money_entity_final, all_results = TRUE,begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "1d5bcdd3e4ef463695e9f1037781fec6",sleep = 1)
+money_entity_API_2 <- as_search(q = money_entity_final, all_results = TRUE,begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "*****",sleep = 1)
 money_entity_API_2_data <- money_entity_API_2$data
 money_entity_API_2_final <- money_entity_API_2_data[,2]
 colnames(money_entity_API_2_final)<-c("money")
 #View(money_entity_API_2_final)
 
 #Data for named entity - date/time
-date_entity_API_2 <- as_search(q = date_entity_final, all_results = TRUE,begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "1d5bcdd3e4ef463695e9f1037781fec6",sleep = 1)
+date_entity_API_2 <- as_search(q = date_entity_final, all_results = TRUE,begin_date = Sys.Date()-1, end_date = Sys.Date(), key = "*****",sleep = 1)
 date_entity_API_2_data <- date_entity_API_2$data
 date_entity_API_2_final <- date_entity_API_2_data[,2]
 colnames(date_entity_API_2_final)<-c("date")
